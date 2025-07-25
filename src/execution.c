5079a1dbeae46d8cac392c3266f009bc64ba6fb5
59378110+sv-LayZ@users.noreply.github.com <59378110+sv-LayZ@users.noreply.github.com>
2025-07-26 01:06:52 +0200

    add: pipes without norms, need to rewrite file desc

--- src/execution.c
+++ src/execution.c
@@ -132,6 +132,17 @@ static int	execute_external_command(char **args)
 	return (0);
 }
 
+static int	count_commands(t_cmd *cmd)
+{
+	int count = 0;
+	while (cmd)
+	{
+		count++;
+		cmd = cmd->next;
+	}
+	return (count);
+}
+
 static int	execute_with_redirections(t_cmd *cmd)
 {
 	pid_t	pid;
@@ -209,6 +220,167 @@ static int	execute_with_redirections(t_cmd *cmd)
 	return (0);
 }
 
+static int	execute_single_cmd_in_pipeline(t_cmd *cmd, int input_fd, int output_fd, int **pipes, int cmd_count)
+{
+	pid_t	pid;
+	int		builtin_index;
+	int		i;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		// Child process
+		// Close all pipe file descriptors except the ones we need
+		for (i = 0; i < cmd_count - 1; i++)
+		{
+			if (pipes[i][0] != input_fd)
+				close(pipes[i][0]);
+			if (pipes[i][1] != output_fd)
+				close(pipes[i][1]);
+		}
+		
+		// Set up input redirection
+		if (input_fd != STDIN_FILENO)
+		{
+			if (dup2(input_fd, STDIN_FILENO) == -1)
+			{
+				perror("dup2 input");
+				exit(1);
+			}
+			close(input_fd);
+		}
+		
+		// Set up output redirection
+		if (output_fd != STDOUT_FILENO)
+		{
+			if (dup2(output_fd, STDOUT_FILENO) == -1)
+			{
+				perror("dup2 output");
+				exit(1);
+			}
+			close(output_fd);
+		}
+		
+		// Handle file redirections
+		if (setup_input_redirection(cmd) == -1)
+			exit(1);
+		if (setup_output_redirection(cmd) == -1)
+			exit(1);
+		
+		// Execute the command
+		builtin_index = is_builtin(cmd->args[0]);
+		if (builtin_index != -1)
+		{
+			exit(execute_builtin(builtin_index, cmd->args));
+		}
+		else
+		{
+			char *executable_path = find_executable_in_path(cmd->args[0]);
+			if (!executable_path)
+			{
+				printf("minishell: %s: command not found\n", cmd->args[0]);
+				exit(127);
+			}
+			if (execve(executable_path, cmd->args, environ) == -1)
+			{
+				perror("execve");
+				free(executable_path);
+				exit(127);
+			}
+		}
+	}
+	return (pid);
+}
+
+static int	execute_pipeline(t_cmd *cmd_list)
+{
+	int		cmd_count;
+	int		**pipes;
+	pid_t	*pids;
+	int		i;
+	int		status;
+	int		last_exit_status = 0;
+	t_cmd	*current;
+
+	cmd_count = count_commands(cmd_list);
+	if (cmd_count == 1)
+	{
+		// Single command, no pipes needed
+		return (execute_with_redirections(cmd_list));
+	}
+
+	// Allocate memory for pipes and process IDs
+	pipes = malloc(sizeof(int *) * (cmd_count - 1));
+	pids = malloc(sizeof(pid_t) * cmd_count);
+	if (!pipes || !pids)
+	{
+		perror("malloc");
+		return (1);
+	}
+
+	// Create all pipes
+	for (i = 0; i < cmd_count - 1; i++)
+	{
+		pipes[i] = malloc(sizeof(int) * 2);
+		if (!pipes[i] || pipe(pipes[i]) == -1)
+		{
+			perror("pipe");
+			return (1);
+		}
+	}
+
+	// Execute each command in the pipeline
+	current = cmd_list;
+	for (i = 0; i < cmd_count; i++)
+	{
+		int input_fd = STDIN_FILENO;
+		int output_fd = STDOUT_FILENO;
+
+		// Set input from previous pipe
+		if (i > 0)
+			input_fd = pipes[i - 1][0];
+
+		// Set output to next pipe
+		if (i < cmd_count - 1)
+			output_fd = pipes[i][1];
+
+		pids[i] = execute_single_cmd_in_pipeline(current, input_fd, output_fd, pipes, cmd_count);
+		current = current->next;
+	}
+
+	// Close all pipe file descriptors in parent
+	for (i = 0; i < cmd_count - 1; i++)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+	}
+
+	// Wait for all child processes
+	for (i = 0; i < cmd_count; i++)
+	{
+		waitpid(pids[i], &status, 0);
+		if (i == cmd_count - 1) // Last command determines exit status
+		{
+			if (WIFEXITED(status))
+				last_exit_status = WEXITSTATUS(status);
+			else
+				last_exit_status = 128 + WTERMSIG(status);
+		}
+	}
+
+	// Free allocated memory
+	for (i = 0; i < cmd_count - 1; i++)
+		free(pipes[i]);
+	free(pipes);
+	free(pids);
+	return (last_exit_status);
+}
+
 int	execute_command(t_cmd *cmd)
 {
 	int	builtin_index;
@@ -217,7 +389,13 @@ int	execute_command(t_cmd *cmd)
 	if (!cmd || !cmd->args || !cmd->args[0])
 		return (0);
 	
-	// Check if there are redirections
+	// Check if this is a pipeline (multiple commands)
+	if (cmd->next)
+	{
+		return (execute_pipeline(cmd));
+	}
+	
+	// Single command - check if there are redirections
 	if (cmd->input_file || cmd->output_file)
 	{
 		return (execute_with_redirections(cmd));
